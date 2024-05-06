2aad5acace61093f3cb2a2499026ca5e7ee3da62
BlairMutisya <tonnyblair09@gmail.com>
2024-05-06 15:00:29 +0300

    Update the Contacts component

---
+++ art-enclave/src/Components/Contact.js
@@ -0,0 +1,20 @@
+// Contacts.js
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mt-8">Contacts</h1>
+      <p className="mt-4">
+        You can contact us through the following channels:
+      </p>
+      <ul className="mt-4">
+        <li>Email: tonnyblair09@gmail.com</li>
+        <li>Phone: +4436785630</li>
+        <li>Address: 123 Museum Street, Nairobi, Kenya</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contacts;
