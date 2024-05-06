46d77f82a01cc00da1b84ed9a7acc449cae292ab
BlairMutisya <tonnyblair09@gmail.com>
2024-05-06 13:42:27 +0300

    Declare the props useState and useEffect

--- art-enclave/src/App.js
+++ art-enclave/src/App.js
@@ -10,3 +10,29 @@ import MyCollection from './MyCollection';
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
   const [collection, setCollection] = useState([]);
+  useEffect(() => {
+    // Fetch data from API and set collection state
+    axios.get('https://api.harvardartmuseums.org/Object', {
+      params: {
+        apikey: '5cac1dd2-ebbc-4567-873d-6ab86590f5e8',
+      },
+    })
+    .then(response => {
+      setCollection(response.data.results);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const addToCollection = (art) => {
+    setCollection([...collection, art]);
+  };
+
+  const removeFromCollection = (id) => {
+    setCollection(collection.filter(art => art.id !== id));
+  };
