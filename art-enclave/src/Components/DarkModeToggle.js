ba9ae8a0930b9c87b4b8f65d285b499afbd6937c
BlairMutisya <tonnyblair09@gmail.com>
2024-05-06 16:05:15 +0300

    Add DarkModeToggle component

---
+++ art-enclave/src/Components/DarkModeToggle.js
@@ -0,0 +1,12 @@
+// DarkModeToggle.js
+import React from 'react';
+
+const DarkModeToggle = ({ darkMode, toggleDarkMode }) => {
+  return (
+    <button onClick={toggleDarkMode}>
+      {darkMode ? 'Light Mode' : 'Dark Mode'}
+    </button>
+  );
+};
+
+export default DarkModeToggle;
